ad596a76b5a5a313cd320a60db4c10eda4ad9add
Sudhanshu <sudhanshukumar4648@gmail.com>
2023-04-10 18:27:06 +0530

    second commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express=require('express');
 const router=express.Router();
 const gravatar=require('gravatar');
 const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const config= require('config');
 const {check, validationResult}=require('express-validator');
 const User=require('../../models/User');
 //@route    POST api/users
@@ -46,8 +48,21 @@ async (req,res)=>{
         await user.save();
 
    //return jsonwebtoken
-        res.send('User registered');
- //   res.send('User route');
+        const payload={
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn:360000},
+            (err,token)=>{
+                if(err)throw err;
+                res.json({token});
+
+            }
+            );
 
     } catch(err){
         console.log(err.message);
