759e91e67586acc8105e02aa8abcdcee53c758bb
Sudhanshu <sudhanshukumar4648@gmail.com>
2023-04-14 03:08:00 +0530

    fourth commit all features of posts is done

---
+++ models/Posts.js
@@ -0,0 +1,63 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const PostSchema=new Schema({
+    //post connected to user
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'users'
+    },
+    text:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String
+    },
+    avatar:{
+        type:String
+
+    },
+   likes:[
+    {
+    //one user only one like
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'users'
+    }
+}
+],
+   comments:[
+    {
+        user:{
+            type:Schema.Types.ObjectId,
+            ref:'users'
+        },
+    
+    text: {
+        type:String,
+        required:true
+    },
+    name:{
+        type:String
+    },
+    avatar:{
+        type:String
+
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+
+}
+   
+
+],
+date:{
+    type:Date,
+    default:Date.now()
+}
+
+});
+
+module.exports=Post=mongoose.model('post',PostSchema);
