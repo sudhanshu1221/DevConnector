a6ca6f9d6c32268a0912901b2f890baa7a3cec7c
Sudhanshu <sudhanshukumar4648@gmail.com>
2023-04-15 19:30:33 +0530

    fifth commit login and sign up feature added

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router,Route,Switch, Routes} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import './App.css';
+
+
+const App=()=> (
+<Router> 
+<Fragment>
+    <Navbar/>   
+    <Routes> 
+    <Route path="/" element={<Landing />} />
+    </Routes>
+    <section className="container">
+    <Routes>
+      <Route path="/register" element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+
+
+    </section>
+
+    
+    </Fragment>
+ </Router>
+   
+);
+
+export default App;
