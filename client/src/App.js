4017c5c99cf155c5eae70e0cca5955146714eed9
Sudhanshu <sudhanshukumar4648@gmail.com>
2023-04-29 02:53:16 +0530

    profile and dashboard done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import React,{Fragment,useEffect} from 'react';
-import {BrowserRouter as Router,Route,Switch, Routes} from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
@@ -7,43 +12,48 @@ import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import './App.css';
 import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setSuthToken';
-import Dashboard from './components/auth/Dashboard';
-// Redux
-import {Provider} from 'react-redux';
+import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard'; // Redux
+import CreateProfile from './components/profile-forms/CreateProfile';
+import { Provider } from 'react-redux';
 import store from './store';
-if(localStorage.token){
+import PrivateRoute from './components/routing/PrivateRoute';
+if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
-const App=()=> {
-  useEffect(()=>{
+const App = () => {
+  useEffect(() => {
     store.dispatch(loadUser());
-  },[]);
-  return  (
-  <Provider store={store}> 
-<Router> 
-<Fragment>
-    <Navbar/>   
-    
-    <Routes> 
-    <Route path="/" element={<Landing />} />
-    </Routes>
-    <section className="container">
-    <Alert />
-    <Routes>
-      <Route path = "/dashboard" element = {<Dashboard />} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
-    </Routes>
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
 
+          <Routes>
 
-    </section>
-
-    
-    </Fragment>
- </Router>
- </Provider>
-)};
+            <Route path='/' element={<Landing />} />
+          </Routes>
+          <section className='container'>
+            <Alert />
+            <Routes>
+              <Route element={<PrivateRoute />}>
+                <Route exact path='/dashboard' element={<Dashboard />} />
+              </Route>
+              <Route element={<PrivateRoute />}>
+                <Route exact path='/create-profile' element={<CreateProfile  />} />
+              </Route>
+              
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,24 +1,44 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-const Landing = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+const Landing = ({ isAuthenticated }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated) {
+      console.log(isAuthenticated);
+      return navigate('/dashboard', { replace: true });
+    }
+  }, [isAuthenticated]);
+
   return (
-    <section className="landing">
-    <div className="dark-overlay">
-      <div className="landing-inner">
-        <h1 className="x-large">Developer Connector</h1>
-        <p className="lead">
-          Create a developer profile/portfolio, share posts and get help from
-          other developers
-        </p>
-        <div className="buttons">
-          <Link to="/register" className="btn btn-primary">Sign Up </Link>
-          <Link to="/login" className="btn btn-primary">Login </Link>
-          
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Developer Connector</h1>
+          <p className='lead'>
+            Create a developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up{' '}
+            </Link>
+            <Link to='/login' className='btn btn-primary'>
+              Login{' '}
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  )
-}
-
-export default Landing
+    </section>
+  );
+};
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+export default connect(mapStateToProps)(Landing);
